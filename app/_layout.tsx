7c86fe8227dfc71300250a3896700a03e0c2c7ec
Otávio Fagundes <otaviofariafagundes1@gmail.com>
2026-03-11 19:28:02 -0300

    tela de login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
 import { Stack } from 'expo-router';
 export default function RootLayout() {
 
-  return (
-      <Stack initialRouteName='entrar'>
-        <Stack.Screen name="(tabs)"/>
-        <Stack.Screen name="entrar"/>
-        <Stack.Screen name="cadastrar"/>
-      </Stack>
-  );
+    return (
+        <Stack initialRouteName='entrar'
+            screenOptions={{
+                headerShown: false,
+            }}>
+            <Stack.Screen name="(tabs)" />
+            <Stack.Screen name="entrar"/>
+            <Stack.Screen name="cadastrarUsuario" />
+        </Stack>
+    );
 }
 
---
+++ context/AuthProvider.tsx
@@ -0,0 +1,45 @@
+
+import React, { createContext } from "react";
+
+
+export const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }: any) => {
+    // async function signIn(email: string, senha: string) {
+    //     // const auth = getAuth();
+    //     try {
+    //         await signInWithEmailAndPassword(auth, email, senha);
+    //         return "ok";
+    //     } catch (error: any) {
+    //         console.error(error.code, error.message);
+    //         return launchServerMessageErro(error);
+    //     }
+    // }
+
+    // //função utilitária
+    // function launchServerMessageErro(e: any): string {
+    //     switch (e.code) {
+    //         case "auth/invalid-credential":
+    //             return "Email inexistente ou senha errada.";
+    //         case "auth/user-not-found":
+    //             return "Usuário não cadastrado.";
+    //         case "auth/wrong-password":
+    //             return "Erro na senha.";
+    //         case "auth/invalid-email":
+    //             return "Email inexistente.";
+    //         case "auth/user-disabled":
+    //             return "Usuário desabilitado.";
+    //         case "auth/email-already-in-use":
+    //             return "Email em uso. Tente outro email.";
+    //         default:
+    //             return "Erro desconhecido. Contate o administrador";
+    //     }
+    // }
+
+    return (
+        <AuthContext.Provider 
+        value={{  }}>
+            {children}
+                </AuthContext.Provider>
+    );
+};
