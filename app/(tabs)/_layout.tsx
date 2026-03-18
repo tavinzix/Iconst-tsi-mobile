eb849ba6882548ad90a2e40aa696d299f439e6a7
Otávio Fagundes <otaviofariafagundes1@gmail.com>
2026-03-18 00:06:17 -0300

    icon na tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,19 @@
 import { Stack, Tabs } from 'expo-router';
+import { Icon, useTheme } from "react-native-paper";
+
 export default function TabsLayout() {
 
     return (
         <Tabs initialRouteName='home'>
-            <Tabs.Screen name="home" />
+            <Tabs.Screen name="home" options={{
+                title: "Home",
+                tabBarIcon: ({ color }) => (
+                    <Icon
+                        source="account-group"
+                        size={20}
+                    />
+                ),
+            }} />
             <Tabs.Screen name="pagina2" />
         </Tabs>
     );
